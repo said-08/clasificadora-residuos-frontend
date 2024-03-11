b5e96ea9bc5c11b7e0be7c67ff0aee884781799a
SAID ELIAS SALCEDO CUPITRA <said.salcedoc@unac.edu.co>
2024-03-11 00:44:47 -0500

    Adding Modal COLOR

--- src/pages/AddImage.jsx
+++ src/pages/AddImage.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { addWasteImage } from "../api/waste.api";
 
 function AddImage() {
   const [desc, setDesc] = useState('');
+  const [main, setMain] = useState('')
   const [images, setImages] = useState([]);
   const [formData, setformData] = useState({
     description: '',
@@ -39,6 +41,7 @@ function AddImage() {
           'description': formData.description
         }, config);
         console.log("image", res);
+        setMain(res.data.label)
     } else {
       console.warn("No se ha seleccionado ninguna imagen");
     }
@@ -92,6 +95,28 @@ function AddImage() {
           <div onClick={handleSubmit} className="btn border border-indigo-500 p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-indigo-500">Clasificar</div>
         </div>
       </div>
+      {
+        main && (
+          <div className="flex justify-center items-center h-screen">
+          <div x-data="{ open: true }">
+            <button x-on:click="open = true" className="px-4 py-2 bg-indigo-500 text-white rounded-md"> Open Modal </button>
+            <div x-show="open" className="fixed inset-0 px-2 z-10 overflow-hidden flex items-center justify-center">
+              <div x-show="open" x-transition:enter="transition-opacity ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition-opacity ease-in duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+              <div x-show="open" x-transition:enter="transition-transform ease-out duration-300" x-transition:enter-start="transform scale-75" x-transition:enter-end="transform scale-100" x-transition:leave="transition-transform ease-in duration-300" x-transition:leave-start="transform scale-100" x-transition:leave-end="transform scale-75" className="bg-white rounded-md shadow-xl overflow-hidden max-w-md w-full sm:w-96 md:w-1/2 lg:w-2/3 xl:w-1/3 z-50">
+                <div className="bg-indigo-500 text-white px-4 py-2 flex justify-between">
+                  <h2 className="text-lg font-semibold">Modal Title</h2>
+                </div>
+                <div className="p-4 bg-slate-800">
+                  <p>Debería ir en la caneca de color {main}</p>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+          )
+      }
+      
     </div>
   )
 }

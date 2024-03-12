36788f798e404fbe6b2ce4c1427043f440365c07
CHRISTIAN ANDRES ASTUDILLO DAVILA <christian.astudilloda@amigo.edu.co>
2024-03-12 15:18:03 -0500

    Adding new category "imagen irreconocible"

--- src/components/Wastecards.jsx
+++ src/components/Wastecards.jsx
@@ -26,27 +26,33 @@ const WasteCards = ({ list }) => {
       </div>
       <div className="grid grid-cols-3 gap-4">
         {list.map((item, index) => (
-          <div
-            key={index}
-            className={`rounded-lg shadow-md p-4 ${
-              item.label === "verde" ? "bg-green-500" : item.label === "blanco" ? "bg-white" : "bg-black"
-            }`}
-          >
-            <div className="relative overflow-hidden bg-cover bg-no-repeat">
-              <img className="rounded-lg sm:m-h-64 md:h-64 w-full" src={item.img} alt="" />
-              <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-gray-700 bg-opacity-50 hover:bg-opacity-0 transition duration-300 ease-in-out"></div>
-            </div>
+          // Agregar condición para evitar agregar la imagen si el label contiene "imagen irreconocible"
+          !item.label.includes("imagen irreconocible") && (
+            <div
+              key={index}
+              className={`rounded-lg shadow-md p-4 ${
+                item.label === "verde" ? "bg-green-500" : item.label === "blanco" ? "bg-white" : "bg-black"
+              }`}
+            >
+              <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                {/* Agregar la imagen solo si el label no contiene "imagen irreconocible" */}
+                {!item.label.includes("imagen irreconocible") && (
+                  <img className="rounded-lg sm:m-h-64 md:h-64 w-full" src={item.img} alt="" />
+                )}
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full bg-gray-700 bg-opacity-50 hover:bg-opacity-0 transition duration-300 ease-in-out"></div>
+              </div>
 
-            <div className="p-2">
-              <h5 className={`mb-2 text-lg font-semibold leading-tight ${
-                item.label === "verde" ? "text-black" : item.label === "blanco" ? "text-gray-800" : "text-white"
-              }`}>{item.description}</h5>
-              <div className="flex justify-between text-sm text-gray-600">
-                <span>{item.date}</span>
-                <span>{item.owner}</span>
+              <div className="p-2">
+                <h5 className={`mb-2 text-lg font-semibold leading-tight ${
+                  item.label === "verde" ? "text-black" : item.label === "blanco" ? "text-gray-800" : "text-white"
+                }`}>{item.description}</h5>
+                <div className="flex justify-between text-sm text-gray-600">
+                  <span>{item.date}</span>
+                  <span>{item.owner}</span>
+                </div>
               </div>
             </div>
-          </div>
+          )
         ))}
       </div>
     </div>
